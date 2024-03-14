0ac21886e8420060112935ad53d745d73ae890c6
Angel Arturo Oramas Rojas <oramasrojas199angelarturo@hotmail.com>
2024-03-14 14:06:02 -0600

    UPLOAD. CREATING THE REPOSITORY FOR THE CRUD

---
+++ src/modules/client/models/repository/client.repository.ts
@@ -0,0 +1,14 @@
+import { Result } from "../commons/result.mode";
+import { Client, CreateClient, UpdateClient } from "../entity/client.entity";
+
+export interface ClientCreateRepository {
+    execute(client: CreateClient): Promise<Result<Client | null>>;
+}
+
+export interface ClientUpdateRepository {
+    execute(id: string, client: UpdateClient): Promise<Result<Client | null>>;
+}
+
+export interface ClientGetByCodeRepository {
+    execute(code: string): Promise<Result<Client[]>>;
+}
